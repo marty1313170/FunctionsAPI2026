6ad61114ad7b09c960268ddeb5862e0dc4185c9a
marty1313170 <martin.han1@education.nsw.gov.au>
2026-02-25 09:24:05 +1100

    Requirments.txt edited and main.py Pokedex uploaded

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # A critical examination: API for Software engineering
-This repo is to outline the Software Design Lifetime schedule,
+This repo is to outline the Software Design Lifetime schedule, and the general use of API'S. This type of API helps people find the important pokemons they like.
 
+# How to run the API
+Run main.py by in terminal by pasting this in: "python main.py"
+
+# Requirements.txt
+Where all the packages are stored
+
+# Languages and libaries used
+Python, requests
---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+
+# API LINK
+base_url = "https://pokeapi.co/api/v2/"
+# ASK 
+def ask_user_for_pokemon_wanted():
+    pokemon_wanted = input("What pokemon would you like to learn about").lower()
+
+    return pokemon_wanted
+
+
+
+def get_pokemon_info(pokemon_wanted):
+    url = f"{base_url}/pokemon/{pokemon_wanted}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retreive{response.pokemon_data}")
+    return
+
+def print_pokemon_info(pokemon_info):
+    print(f'Name: {pokemon_info["name"].capitalize()}')
+    print(f'Height: {pokemon_info["height"]}')
+    print(f'Weight: {pokemon_info["weight"]}')
+    print(f': {pokemon_info["age"]}')
+    return
+
+# PERFORM FUNCTIONS
+name = ask_user_for_pokemon_wanted()
+data = get_pokemon_info(name)
+print_pokemon_info(data)
+
+    
+
+
+    
+ 
